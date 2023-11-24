62b5f63e39d8b62a9c5cb2c6f26c3e796632ae78
Andrew <andrewwasfy24@gmail.com>
2023-11-24 15:18:06 +0200

    add newest books use case

--- lib/Features/home/domain/usecases/fetch_newest_books_usecase.dart
+++ lib/Features/home/domain/usecases/fetch_newest_books_usecase.dart
@@ -2,15 +2,15 @@ import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/core/usecases/use_case.dart';
-import 'package:either_dart/either.dart';
+import 'package:either_dart/src/either.dart';
 
-class FeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
-  final HomeRepo homerepo;
+class NewestBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
+  final HomeRepo homeRepo;
 
-  FeaturedBooksUseCase(this.homerepo);
+  NewestBooksUseCase(this.homeRepo);
   @override
   Future<Either<Failrue, List<BookEntity>>> call([NoParam? param]) async {
-    // future edits
-    return await homerepo.fetchFeaturedBooks();
+    // future edits 
+    return await homeRepo.fetchNewestBooks();
   }
 }
--- lib/core/usecases/use_case.dart
+++ lib/core/usecases/use_case.dart
@@ -2,7 +2,7 @@ import 'package:bookly/core/errors/failure.dart';
 import 'package:either_dart/either.dart';
 
 abstract class UseCase<Type, Param> {
-  Future<Either<Failrue, Type>> call([Param param]);
+  Future<Either<Failrue, Type>> call ([Param param]);
 }
 class NoParam {}
 
